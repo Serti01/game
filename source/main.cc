b296628dcbe42810626108291b1b2d345f2f0ba2
devsamoyo <samoyo@protonmail.com>
2021-07-27 00:03:29 -0400

    update main.cc

--- source/main.cc
+++ source/main.cc
@@ -6,13 +6,13 @@ int main(int argc, char **argv) {
   handleArgs(argc, argv);
 
   // Init ncurses
-  // initscr();
-  // nocbreak();
-  // noecho();
-  // nonl();
-  // intrflush(stdscr, FALSE);
-  // keypad(stdscr, TRUE);
-  // clear();
+  initscr();
+  nocbreak();
+  noecho();
+  nonl();
+  intrflush(stdscr, FALSE);
+  keypad(stdscr, TRUE);
+  clear();
 
   printf("hello!");
   
